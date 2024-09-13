e3c9848e6ce28e135bdb3b690a9f7d8ebdbc672b
Johannes Merkle <johannesmerkle@gmx.de>
2024-09-13 15:15:07 +0200

    increase number of fetched emojis

--- grab_slack_emojis.sh
+++ grab_slack_emojis.sh
@@ -15,9 +15,9 @@ emoji_data=$(curl -s "https://$org_name.slack.com/api/emoji.adminList" \
   -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
   -H 'content-type: multipart/form-data' \
   -H "cookie: d=$auth_cookie_value" \
-  -F token=$token_value \
+  -F token="$token_value" \
   -F page=1 \
-  -F count=10 \
+  -F count=5000 \
   -F sort_by=name \
   -F sort_dir=asc | \
   jq -r .emoji)
